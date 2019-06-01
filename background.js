def67694adcfe081af6b359f77c45c2c1c290f5b
Imed Adel <aadelimed@gmail.com>
2019-06-01 18:59:13 +0200

    :recycle: add background script for onClick

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.browserAction.onClicked.addListener((tab) => {
+	//Fired when User Clicks ICON
+	// if (tab.url.indexOf('https://www.google.co.in/') != -1) {
+		// Inspect whether the place where user clicked matches with our list of URL
+		chrome.tabs.executeScript(
+			tab.id,
+			{
+				file: 'content.js',
+			},
+			() => {
+				// Execute your code
+				console.log('Script Executed .. ') // Notification on Completion
+			}
+		)
+	// }
+})
