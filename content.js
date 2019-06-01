b3485792e8437ea900ec1560ff9a441e34dcacf4
Imed Adel <aadelimed@gmail.com>
2019-06-01 20:24:44 +0200

    :recycle: add isolation for media

--- content.js
+++ content.js
@@ -5,14 +5,20 @@ chrome.runtime.onMessage.addListener((request,sender, sendResponse) => {
 	if(request) {
 		console.log('set to light')
 		document.body.removeChild(document.body.children[0])
+		document.head.removeChild(document.head.lastChild)
 	} else {
 		console.log('set to dark')
-		// let darkDivID = generateID()
-
+		
 		const darkDiv = document.createElement('div')
-		// darkDiv.id = darkDivID
-		darkDiv.style.cssText = `position: fixed; height: 100vh; width: 100vw; background-color: color('sugarcane'); mix-blend-mode: exclusion; pointer-events: none;`
-		// document.body.appendChild(darkDiv)
+		darkDiv.style.cssText = `width: 100vw; height: 100vh; position: fixed; top: 0; left: 0; background: white; mix-blend-mode: difference; z-index: 2147483646;`
+		// Insert blending div at the top
 		document.body.insertAdjacentElement('afterbegin', darkDiv)
+
+		// CSS to isolate media
+		const css = `img, picture, video, .emoji, iframe {isolation: isolate; mix-blend-mode: difference !important; }`
+		const isolationStyle = document.createElement('style')
+		isolationStyle.type = `text/css`
+		isolationStyle.appendChild(document.createTextNode(css))
+		document.head.appendChild(isolationStyle)
 	}
 })
