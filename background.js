75379e07400c98971d133af3f5d6a200c53c56e3
Imed Adel <aadelimed@gmail.com>
2019-06-01 19:29:14 +0200

    :recycle: track number of clicks

--- background.js
+++ background.js
@@ -1,16 +1,21 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+let darkToggle = false
+const setDarkToggle = () => darkToggle = !darkToggle
+
+chrome.browserAction.onClicked.addListener(tab => {
 	//Fired when User Clicks ICON
 	// if (tab.url.indexOf('https://www.google.co.in/') != -1) {
-		// Inspect whether the place where user clicked matches with our list of URL
-		chrome.tabs.executeScript(
-			tab.id,
-			{
-				file: 'content.js',
-			},
-			() => {
-				// Execute your code
-				console.log('Script Executed .. ') // Notification on Completion
-			}
-		)
+	// Inspect whether the place where user clicked matches with our list of URL
+	// chrome.tabs.executeScript(
+	// 	tab.id,
+	// 	{
+	// 		file: 'content.js',
+	// 	},
+	// 	() => {
+	// 		// Execute your code
+	// 		console.log('Script Executed .. ') // Notification on Completion
+	// 	}
+	// )
 	// }
+	chrome.tabs.sendMessage(tab.id, darkToggle)
+	setDarkToggle()
 })
--- content.js
+++ content.js
@@ -1,1 +1,9 @@
 console.log('Try this yo')
+
+chrome.runtime.onMessage.addListener((request,sender, sendResponse) => {
+	if(request) {
+		console.log('set to light')
+	} else {
+		console.log('set to dark')
+	}
+})
